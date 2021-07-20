553b9331562ccba620349d331fd0528fb3cc5f97
disha2402 <dvpishavadia24@gmail.com>
2021-07-20 18:58:39 +0530

    pref center work in progress

--- dist/listeners.js
+++ dist/listeners.js
@@ -1,8 +1,16 @@
 import DynamicContentListeners from './dynamicContent/dynamicContent.listeners';
+import PreferenceCenterListeners from './preferenceCenter/preferenceCenter.listeners';
+
 export default ((editor, options) => {
   const dynamicContentTabs = [];
   const dcListener = new DynamicContentListeners(editor, dynamicContentTabs);
-  dcListener.onLoad(); // @todo is this needed? why do we copy the original content?
+  dcListener.onLoad(); 
+
+  const preferenceCenterTabs = [];
+
+  const pcListener = new PreferenceCenterListeners(editor, preferenceCenterTabs);
+  pcListener.onLoad();
+  // @todo is this needed? why do we copy the original content?
   // this.editor.on('run:mautic-editor-email-mjml-close:before', () => {
   //   mQuery('textarea.builder-html').val(this.getBody());
   // });
